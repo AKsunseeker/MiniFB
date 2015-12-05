0836f5734409100209cac9065e199baa9e8b7ea7
Ander Rockstad <ander.rockstad@gmail.com>
2015-12-05 15:32:30 -0700

    CRUD actions on all posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,55 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all 
+    @users = User.all
   end
 
   def new
+    @post = Post.new
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    if post.destroy 
+      redirect_to posts_path 
+      
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:content, :user_id)
+    end
+
+
+
+
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151204222746) do
+ActiveRecord::Schema.define(version: 20151205013603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,12 +21,15 @@ ActiveRecord::Schema.define(version: 20151204222746) do
     t.text     "opinion"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "post_id"
+    t.integer  "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
